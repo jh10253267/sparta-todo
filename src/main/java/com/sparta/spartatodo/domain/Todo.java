e7c8e896be1ee38dcaddedd27acc1a04f50e0724
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-17 19:56:24 +0900

    #9-기본 엔티티 작성 및 기타 수정
    APIUser 와 Todo 엔티티 작성, 시큐리티 컨피그파일 컨피그 디렉토리로 이동

---
+++ src/main/java/com/sparta/spartatodo/domain/Todo.java
@@ -0,0 +1,39 @@
+package com.sparta.spartatodo.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="tbl_todo_api")
+public class Todo extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+    @Column(length = 100, nullable = false)
+    private String title;
+    @Column(length=500, nullable = false)
+    private String content;
+    @Column(length = 50, nullable = false)
+    private String writer;
+    private boolean complete;
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void changeComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+}
