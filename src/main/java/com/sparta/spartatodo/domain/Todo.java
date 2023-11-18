0f86ab1e695314282731099bb8b0e5aa9c369748
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-18 23:53:42 +0900

    #17-TodoService구현을 위한 DTO수정 및 엔티티 소폭 수정
    DTO의 불필요한 필드값 수정 및 엔티티 필드 수정, 업데이트 기능을 위한 메소드 추가

--- src/main/java/com/sparta/spartatodo/domain/Todo.java
+++ src/main/java/com/sparta/spartatodo/domain/Todo.java
@@ -1,31 +1,27 @@
 package com.sparta.spartatodo.domain;
 
-
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @Getter
 @ToString
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@Table(name="tbl_todo_api")
-public class Todo extends BaseEntity{
+@NoArgsConstructor
+@Table(name = "tbl_todo_api")
+public class Todo extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tno;
     @Column(length = 100, nullable = false)
     private String title;
-    @Column(length=500, nullable = false)
+    @Column(length = 500, nullable = false)
     private String content;
     @Column(length = 50, nullable = false)
     private String writer;
-    @ColumnDefault("0")
-    @Column(columnDefinition = "TINYINT(1)")
     private boolean complete;
 
     public void changeTitle(String title) {
@@ -34,7 +30,9 @@ public class Todo extends BaseEntity{
     public void changeContent(String content) {
         this.content = content;
     }
-
+    public void changeWriter(String writer) {
+        this.writer = writer;
+    }
     public void changeComplete(boolean complete) {
         this.complete = complete;
     }
--- src/main/java/com/sparta/spartatodo/dto/TodoDTO.java
+++ src/main/java/com/sparta/spartatodo/dto/TodoDTO.java
@@ -1,12 +1,9 @@
 package com.sparta.spartatodo.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Data
 @Builder
@@ -17,13 +14,5 @@ public class TodoDTO {
     private String title;
     private String content;
     private String writer;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/seoul")
-    private LocalDate createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/seoul")
-    private LocalDate modifiedAt;
-
     private boolean complete;
-
-
 }
