296234ca0d673eb03033646e32b184063a387f0a
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-18 20:29:19 +0900

    #12-TokenCheckFilter, AccessTokenException작성
    토큰인증이 필요한 경로에 대해 브라우저가 전송하는 AccessToken을 추출하고 검증하는 기능 구현
    요청 헤더에서 'Authorization' 헤더를 추출햐고 검증. 예외는 enum을 구현한 AccessTokenException을 이용해 예외 케이스 정의. 예외가 발생했을 때 브라우저에 에러메시지와 상태코드를 전송하도록 함.

---
+++ src/main/java/com/sparta/spartatodo/exception/AccessTokenException.java
@@ -0,0 +1,59 @@
+package com.sparta.spartatodo.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
+public class AccessTokenException extends RuntimeException {
+
+    TOKEN_ERROR token_error;
+
+    public enum TOKEN_ERROR {
+        UNACCEPT(401, "Token is null or too short"),
+        BADTYPE(401, "Token type Bearer"),
+        MALFORM(403, "Malformed Token"),
+        BADSiGN(403, "BadSignatured Token"),
+        EXPIRED(403, "Expired Token");
+
+        private int status;
+        private String message;
+
+        TOKEN_ERROR(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public AccessTokenException(TOKEN_ERROR error) {
+        super(error.name());
+        this.token_error = error;
+    }
+
+    public void sendResponseError(HttpServletResponse response) throws JsonProcessingException {
+        response.setStatus(token_error.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String responseStr = objectMapper.writeValueAsString(Map.of("message", token_error.getMessage(), "time", new Date()));
+
+        try {
+            response.getWriter().println(responseStr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
