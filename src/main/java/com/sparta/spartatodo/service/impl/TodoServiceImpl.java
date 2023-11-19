5a6c4d1cb7d443101893d965aa9d6502a7c3a692
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-19 19:11:20 +0900

    #19-DTO를 추가함으로써 Service에도 영향이 갔고 해당 부분 수정
    DTO 클래스를 변경함으로써 연쇄적으로 생긴 서비스 파라미터 변경사항 반영,  컨트롤러에서 register메소드를 통해 가입하고 서비스 내부에서 response객체를 만들어 반환하도록 수정

--- src/main/java/com/sparta/spartatodo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/sparta/spartatodo/service/impl/TodoServiceImpl.java
@@ -1,7 +1,8 @@
 package com.sparta.spartatodo.service.impl;
 
 import com.sparta.spartatodo.domain.Todo;
-import com.sparta.spartatodo.dto.TodoDTO;
+import com.sparta.spartatodo.dto.TodoRegisterRequestDTO;
+import com.sparta.spartatodo.dto.TodoResponseDTO;
 import com.sparta.spartatodo.repository.TodoRepository;
 import com.sparta.spartatodo.service.TodoService;
 import lombok.RequiredArgsConstructor;
@@ -19,29 +20,30 @@ import java.util.stream.Collectors;
 public class TodoServiceImpl implements TodoService {
     private final TodoRepository todoRepository;
     private final ModelMapper modelMapper;
+
     @Override
-    public Long register(TodoDTO todoDTO) {
-        Todo todo = modelMapper.map(todoDTO, Todo.class);
+    public TodoResponseDTO register(TodoRegisterRequestDTO todoRequestDTO) {
+        Todo todo = modelMapper.map(todoRequestDTO, Todo.class);
+        Todo resultTodo = todoRepository.save(todo);
+        TodoResponseDTO todoResponseDTO = modelMapper.map(resultTodo, TodoResponseDTO.class);
 
-        Long tno = todoRepository.save(todo).getTno();
-
-        return tno;
+        return todoResponseDTO;
     }
 
     @Override
-    public TodoDTO read(Long tno) {
+    public TodoResponseDTO read(Long tno) {
         Optional<Todo> result = todoRepository.findById(tno);
         Todo todo = result.orElseThrow();
-        return modelMapper.map(todo, TodoDTO.class);
+        return modelMapper.map(todo, TodoResponseDTO.class);
 
     }
 
     @Override
-    public List<TodoDTO> readAll() {
+    public List<TodoResponseDTO> readAll() {
         List<Todo> todos = todoRepository.findAll();
 
         return todos.stream()
-                .map(todo -> modelMapper.map(todo, TodoDTO.class))
+                .map(todo -> modelMapper.map(todo, TodoResponseDTO.class))
                 .collect(Collectors.toList());
     }
 
@@ -51,24 +53,10 @@ public class TodoServiceImpl implements TodoService {
     }
 
     @Override
-    public void modify(TodoDTO todoDTO) {
-        Optional<Todo> result = todoRepository.findById(todoDTO.getTno());
-        Todo todo = result.orElseThrow();
+    public void modify(TodoRegisterRequestDTO todoRequestDTO) {
 
-        String title = todoDTO.getTitle();
-        String content = todoDTO.getContent();
-        String writer = todoDTO.getWriter();
-        boolean complete = todoDTO.isComplete();
-        log.info("input DTO value: " + complete);
-
-        todo.changeTitle(title);
-        todo.changeContent(content);
-        todo.changeWriter(writer);
-        todo.changeComplete(complete);
-
-        //더티체킹에 의해 자동으로 수정된다는 걸 알지만 명시적으로 선언해줬습니다. update를 제외한 crud 기능들은 레포지토리를 통해서
-        //DB에 반영이 되는데 업데이트 또한 명시적으로 실행해주는 게 어떨까 생각이 들었습니다.
-        //또 컨트롤러단 -> 서비스계층 -> 영속계층으로 요청의 흐름이 진행되기에 처리의 적법성?에 대한 생각이 들어서 이렇게 짜보았습니다.
-        todoRepository.save(todo);
     }
+
+
 }
+
