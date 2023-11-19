59116f105d4b38876750bb9ea3b0872bfd83aad9
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-20 07:09:39 +0900

    #33-댓글 수정기능을 위해 change메소드 생성

--- src/main/java/com/sparta/spartatodo/domain/Reply.java
+++ src/main/java/com/sparta/spartatodo/domain/Reply.java
@@ -22,4 +22,8 @@ public class Reply extends BaseEntity{
     private String replyText;
     private String replyWriter;
 
+    public void changeText(String replyText) {
+        this.replyText = replyText;
+    }
+
 }
